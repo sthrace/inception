a77541bb370a224ec444aa36be7f0d0a465edc79
sthrace <inbox.d299@gmail.com>
2021-06-28 16:47:19 +0300

    commit

--- srcs/requirements/mariadb/conf/mysql.sh
+++ srcs/requirements/mariadb/conf/mysql.sh
@@ -1,4 +1,12 @@
-service mysql start
-mysql -u root
-mysql --execute="CREATE DATABASE wpdb; \
-                GRANT ALL ON wordpress.* TO 'sthrace'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION; \                FLUSH PRIVILEGES;"
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+    mysql_install_db
+    service mysql start
+
+    mysql -e "CREATE USER '${MYSQL_USER}'@'sthrace.42.fr' identified by '${MYSQL_PASSWORD}';" &&\
+    mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" &&\
+    mysql -e "FLUSH PRIVILEGES;"
+    service mysql stop
+fi
+mysqld
+
--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -1,1 +1,10 @@
-service php7.3-fpm start
+sleep 10;
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    wp core --allow-root download --locale=en_US --force
+    sleep 10;
+    while [ ! -f /var/www/wordpress/wp-config.php ]; do
+        wp core config --allow-root --dbname=wp --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+    done
+    wp core install --allow-root --url='sthrace.42.fr' --title='School 21 Inception project' --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email=$WORDPRESS_EMAIL --path='/var/www/wordpress';
+fi
+php-fpm7.3 --nodaemonize
